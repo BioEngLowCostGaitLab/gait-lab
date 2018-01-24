d98838ea0f8fd95e3490637206ca350efb36bf06
anttilankinen <ail215@ic.ac.uk>
2018-01-24 12:03:53 +0000

    classifier running on opencv

--- detection/detect.py
+++ detection/detect.py
@@ -15,10 +15,10 @@ import argparse
 from os.path import join
 import os
 
-MIN_BLOBS = 5
+MIN_BLOBS = 10
 MAX_BLOBS = 10
 MIN_THRESHOLD = 1e3
-MAX_THRESHOLD = 5e4
+MAX_THRESHOLD = 50
 
 def get_args():
     # user defined arguments, video file in onedrive, ask Antti
@@ -32,9 +32,9 @@ def get_args():
     ap.add_argument("-m", "--model", type=str,
     default=join(os.getcwd(), 'resources', 'MobileNetSSD_deploy.caffemodel'),
 	help="path to Caffe pre-trained model")
-    ap.add_argument("-c", "--confidence", type=float, default=0.2,
+    ap.add_argument("-c", "--confidence", type=float, default=0.3,
 	help="minimum probability to filter weak detections")
-    ap.add_argument("-r", "--rec", type=bool, default=False,
+    ap.add_argument("-r", "--rec", type=bool, default=True,
 	help="option to draw rectangle")
     ap.add_argument("-f", "--freq", type=int, default=30,
 	help="number of images to show per 1 second of footage")
@@ -46,16 +46,15 @@ def get_args():
 
     return vars(ap.parse_args())
 
-def evaluate_net(net, frame, n_frame, last_found, opts):
+def evaluate_ssd(ssd, frame, n_frame, last_found, opts, startX, startY, endX, endY):
     # args: network, frame, number of passed frames, number of frame in which person was last found, opts
     # returns: top left corner and bottom right corner of rectangle in which person lies,
     # number of frame in which person was last found
     blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 0.007843,
     (300, 300), 127.5)
     found = False
-    net.setInput(blob)
-    detections = net.forward()
-
+    ssd.setInput(blob)
+    detections = ssd.forward()
     for i in np.arange(0, detections.shape[2]):
         confidence = detections[0, 0, i, 2]
         if confidence > opts['confidence']:
@@ -71,7 +70,14 @@ def evaluate_net(net, frame, n_frame, last_found, opts):
     if found:
         return startX, startY, endX, endY, last_found, conf_f
     else:
-        return 0, 0, 0, 0, last_found, 0
+        return startX, startY, endX, endY, last_found, 0
+
+def evaluate_classifier(classifier, kp, frame):
+    images = get_keypoint_images(kp, frame)
+    input = cv2.dnn.blobFromImages(images)
+    classifier.setInput(input)
+    output = classifier.forward()
+    return output
 
 def overlap(p1, p2):
     # return True if two keypoints overlap
@@ -99,19 +105,19 @@ def delete_overlap(kp):
 
     return filtered
 
-def filter_kp(kp, w):
+def filter_kp(kp, h, w):
     # filtering criteria: must be smaller than maximum diameter
-    max_size = w * 0.015  # maximum diameter of keypoint
+    max_size = w * 0.05  # maximum diameter of keypoint
     filtered = []
 
     for i in range(len(kp)):
-        if not kp[i].size > max_size:
+        if kp[i].size < max_size:# and kp[i].pt[1] > 0.3 * h :
             # filtering criteria met
             filtered.append(kp[i])
 
     for i in range(len(filtered)):
         # move the keypoint to match their location in the image
-        filtered[i].pt = (filtered[i].pt[0] + startX, filtered[i].pt[1] + startY)
+        filtered[i].pt = (filtered[i].pt[0] + startX, filtered[i].pt[1])
 
     filtered = delete_overlap(filtered)
     return filtered
@@ -124,6 +130,28 @@ def save_keypoints(kp, frame, n_frame, opts, final_size=32):
                             int(keypoint.pt[0]) - final_size / 2:int(keypoint.pt[0]) + final_size / 2]
         cv2.imwrite(join(opts['dir'], '%s_%d_%d.png' % (opts['video'].split('\\')[-1], n_frame, i)), final_image)
 
+def get_keypoint_images(kp, frame):
+    out = []
+    for i, keypoint in enumerate(kp):
+        image = frame[int(keypoint.pt[1]) - 16:int(keypoint.pt[1]) + 16,
+                            int(keypoint.pt[0]) - 16:int(keypoint.pt[0]) + 16]
+        out.append(image)
+    return out
+
+def separate(preds, kp):
+    markers = []
+    ghosts = []
+    for i in range(len(preds)):
+        if preds[i] > 0.5:
+            markers.append(kp[i])
+        else:
+            ghosts.append(kp[i])
+    print(len(markers))
+    return markers, ghosts
+
+
+
+
 
 
 
@@ -132,8 +160,8 @@ if opts['freq'] > 60:
     opts['freq'] = 60
 
 cap = cv2.VideoCapture(opts['video'])
-net = cv2.dnn.readNetFromCaffe(opts['prototxt'], opts['model']) # SSD person detector
-#test = cv2.dnn.readNetFromTensorflow
+ssd = cv2.dnn.readNetFromCaffe(opts['prototxt'], opts['model']) # SSD person detector
+classifier = cv2.dnn.readNetFromTensorflow(join(os.getcwd(), 'frozen_model.pb')) # blob classifier
 
 threshold = MAX_THRESHOLD
 
@@ -148,23 +176,34 @@ if opts['save'] == True:
         os.mkdir(opts['dir'])
     except:
         pass
-
+startX, startY, endX, endY = 0, 0, 0, 0
+kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
 while(True):
     ret, frame = cap.read()
-    frame = cv2.resize(frame, (1920, 1080))
-    grey_frame = cv2.equalizeHist(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY).astype(np.uint8))
+    frame = cv2.resize(frame, (1280, 720))
+    #frame = cv2.flip(frame, 0)
+    grey_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY).astype(np.uint8)
     (h, w) = frame.shape[:2]
     if n_frame > 900 and n_frame % (60 / opts['freq']) == 0: # because for now first 800 frames are not interesting
                                                              # also analyse only opts['freq'] frames per second of video
-        startX, startY, endX, endY, last_found, confidence = evaluate_net(net, frame, n_frame, last_found, opts)
-        if confidence > opts['confidence']:
-            grey_frame = grey_frame[startY:endY, startX:endX]
+        startX, startY, endX, endY, last_found, confidence = evaluate_ssd(ssd, frame, n_frame, last_found, opts, startX, startY, endX, endY)
+        startX, endX = startX - int(0.1 * (endX - startX)), endX + int(0.1 * (endX - startX))
+        if (endX > w):
+            endX = w
+        if True:
+            grey_frame = grey_frame[:, startX:endX]
+            #grey_frame = cv2.filter2D(grey_frame, -1, kernel)
             while True:
                 # adaptive filtering:
                 # we want to find between 5-10 blobs in this particular video (2-4 times the actual number of markers visible)
                 # the detection threshold of the SURF detector is adjusted according to that withing reasonable range
                 kp = detector.detect(grey_frame, None)
-                kp = filter_kp(kp, w)
+                kp = filter_kp(kp, h, w)
+                if len(kp) >= MAX_BLOBS:
+                    kp = kp[:MAX_BLOBS]
+                if (len(kp) == 0):
+                    print("no points found")
+                break
                 if len(kp) >= MIN_BLOBS and len(kp) <= MAX_BLOBS:
                     break
                 elif threshold > MIN_THRESHOLD and len(kp) < MIN_BLOBS:
@@ -180,9 +219,15 @@ while(True):
             if opts['save'] == True:
                 save_keypoints(kp, frame, n_frame, opts)
             if opts['rec'] == True:
-                cv2.rectangle(frame, (startX, startY), (endX, endY),
+                cv2.rectangle(frame, (startX, 0), (endX, h),
 			(0, 255, 0), 10)
-            frame = cv2.drawKeypoints(frame,kp,None,(0, 255 ,0),4)
+            if (len(kp) > 0):
+                pred = evaluate_classifier(classifier, kp,  frame)
+                markers, ghosts = separate(pred, kp)
+                frame = cv2.drawKeypoints(frame,markers,None,(0, 255 ,0),4)
+                frame = cv2.drawKeypoints(frame,ghosts,None,(0, 0 ,255),4)
+            else:
+                frame = cv2.drawKeypoints(frame,kp,None,(0, 255 ,0),4)
     if n_frame % (60 / opts['freq']) == 0:
         cv2.imshow("output", cv2.resize(frame, (640, 360)))
     n_frame +=1
