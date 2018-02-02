a90afb361739433764214b2577c0eabc3eb9d220
Antti Lankinen <ail215@ic.ac.uk>
2018-02-02 16:06:15 +0000

    nn functions module

---
+++ detection/tffunctions.py
@@ -0,0 +1,32 @@
+import tensorflow as tf
+
+def _parse_function(filename, label):
+    image_string = tf.read_file(filename)
+    image_decoded = tf.image.decode_image(image_string)
+    sess = tf.Session()
+    return image_decoded, label
+
+def augment(images, batch_size):
+    transformed_images = []
+    for i in range(batch_size):
+        transformed_images.append(
+        tf.expand_dims(
+                       tf.image.random_flip_up_down(
+                       tf.image.random_flip_left_right(images[i, :, :, :]), 0), 0))
+    out = tf.convert_to_tensor(transformed_images)
+    #out = tf.image.resize_images(out, [24, 24])
+    return out
+
+
+def Net(x):
+    fSize = 32
+    # Hidden layer with RELU activation
+    x = tf.layers.conv2d(x, fSize, 3, strides=(2,2),
+    activation=tf.nn.relu)
+    x = tf.layers.conv2d(x, 2 * fSize, 3, strides=(2,2),
+    activation=tf.nn.relu)
+    x = tf.reshape(x, [1, 100])
+    x = tf.contrib.layers.fully_connected(100, 30)
+    x = tf.contrib.layers.fully_connected(30, 1, activation_fn=None)
+    x = tf.nn.sigmoid(x)
+    return x
