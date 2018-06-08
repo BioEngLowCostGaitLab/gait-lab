566a9873e5541b4460aca1af54a02862ec1587d5
Joearrowsmith <joearrowsmith98@gmail.com>
2018-06-08 02:57:15 +0100

    annotated second video

--- tracking/dev_area/aws/cnn_to_points.py
+++ tracking/dev_area/aws/cnn_to_points.py
@@ -14,6 +14,12 @@ from keras import regularizers
 from keras import backend as K
 from keras.callbacks import EarlyStopping, ModelCheckpoint
 
+# Nicely formatted time string
+def hms_string(sec_elapsed):
+    h = int(sec_elapsed / (60 * 60))
+    m = int((sec_elapsed % (60 * 60)) / 60)
+    s = sec_elapsed % 60
+    return "{}:{:>02}:{:>05.2f}".format(h, m, s)
 
 import pickle
 
